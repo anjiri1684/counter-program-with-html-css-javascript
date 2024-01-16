1edc27bc5aeb183cf2f5fbad0391bb495af9e0a5
vincent anjiri <vincentanjiri12@gmail.com>
2024-01-16 17:07:40 +0300

    number-guessing-app

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 
-const decreaseBtn = document.getElementById("decreaseBtn");
-const resetBtn = document.getElementById("resetBtn");
-const increaseBtn = document.getElementById("increaseBtn");
-const countLabel = document.getElementById("countLabel");
+let attempts = 0;
+let guess;
+let running = true;
 
-let count = 0;
+while (running) {
 
-increaseBtn.onclick = function(){
-    count++;
-    countLabel.textContent  = count;
-}
+    guess = window.prompt(`Guess a number between ${minNum} - ${maxNum}`);
+    guess = Number(guess);
 
-decreaseBtn.onclick = function(){
-    count--;
-    countLabel.textContent  = count;
-}
+    if (isNaN(guess)) {
+        window.alert("Please Enter a valid number");
+    } else if (guess < minNum || guess > maxNum) {
+        window.alert("Please Enter a valid number");
+    } else {
+        attempts++;
+        if (guess < answer) {
+            window.alert("TOO LOW! TRY AGAIN!")
+        } else if (guess > answer) {
+            window.alert("TOO HIGH! TRY AGAIN!!");
+        } else {
+            window.alert(`CORRECT! The answer was ${answer}. It took you ${attempts} attemps`);
+            running = false;
+        }
+    running = false;
 
-resetBtn.onclick = function(){
-    count = 0;
-    countLabel.textContent  = count;
+    }
+
 }
